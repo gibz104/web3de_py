055b3159e3d6f9108fc00090ca321f81ffaadd27
gibz104 <gibz104@users.noreply.github.com>
2022-02-05 22:11:23 -0600

    start Ganache handler

---
+++ handlers/Ganache.py
@@ -0,0 +1,73 @@
+import platform
+import subprocess
+
+# print(system)
+# print(release)
+
+# print(result.stdout)
+# print(result.stderr)
+# print(result.returncode)
+
+
+class Ganache:
+    def __init__(self):
+        self.system = platform.system()
+        self.release = platform.release()
+
+        # Installs Ganache if not installed (uses npm or Yarn)
+        if not self.check_ganache():
+            if self.check_npm():
+                self.install_ganache('npm')
+                print('LOG: Installed Ganache using npm.')
+            elif self.check_yarn():
+                self.install_ganache('yarn')
+                print('LOG: Installed Ganache using Yarn.')
+            else:
+                raise Exception('Please install NPM or Yarn package manager.')
+
+    def check_ganache(self):
+        check = subprocess.run("ganache-cli --version", shell=True, capture_output=True, text=True)
+        if check.returncode == 0:
+            return True
+        else:
+            return False
+
+    def check_nodejs(self):
+        check = subprocess.run("node -v", shell=True, capture_output=True, text=True)
+        print(check.returncode)
+        if check.returncode == 0:
+            return True
+        else:
+            return False
+
+    def check_npm(self):
+        check = subprocess.run("npm -v", shell=True, capture_output=True, text=True)
+        if check.returncode == 0:
+            return True
+        else:
+            return False
+
+    def check_yarn(self):
+        check = subprocess.run("yarn --version", shell=True, capture_output=True, text=True)
+        if check.returncode == 0:
+            return True
+        else:
+            return False
+
+    def install_ganache(self, pm: str):
+        result = None
+        if pm == 'npm':
+            result = subprocess.run('npm install -g ganache-cli', shell=True, capture_output=True, text=True)
+        elif pm == 'yarn':
+            result = subprocess.run('yarn global add ganache-cli', shell=True, capture_output=True, text=True)
+
+        if result.returncode != 0 or result is None:
+            raise Exception('Cannot install Ganache-CLI.')
+
+    def fork_mainnet(self):
+        # ganache-cli --fork http://localhost:8545 --port 8555
+        pass
+
+
+# ganache = Ganache()
+
