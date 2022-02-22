3153d74ed5d7a876874540b583583c13e41d2cb0
gibz104 <gibz104@users.noreply.github.com>
2022-02-21 21:08:41 -0600

    update uni functionality and example

--- examples/uniswap_pool_prices.py
+++ examples/uniswap_pool_prices.py
@@ -1,10 +1,24 @@
 from handlers.UniswapV2 import UniswapV2
 from utils.ERC20 import ERC20Handler
 
-
 uni = UniswapV2()
-data = uni.get_reserves("0xBb2b8038a1640196FbE3e38816F3e67Cba72D940")
-print(f"ETHBTC Price: {(data['reserve1'] * 10 ** 18) / (data['reserve0'] * 10 ** 8)}")
-
 erc20 = ERC20Handler()
-print(erc20.get_decimals("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"))
+
+# Set Uni v2 pool
+unipool = "0xBb2b8038a1640196FbE3e38816F3e67Cba72D940"
+
+# Get reserves of Uni v2 pool
+reserves = uni.get_reserves(unipool)
+
+# Get token addresses in Uni v2 pool
+tokens = uni.get_pool_tokens(unipool)
+
+# Get decimal places of tokens in Uni v2 pool
+token0decimals = erc20.get_decimals(tokens['token0'])
+token1decimals = erc20.get_decimals(tokens['token1'])
+
+# Calculate exchange rate of the pool
+pool_price = (reserves['reserve1'] * 10 ** token1decimals) / (reserves['reserve0'] * 10 ** token0decimals)
+
+# print exchange rate
+print(f"ETHBTC Price: {pool_price}")
--- handlers/UniswapV2.py
+++ handlers/UniswapV2.py
@@ -6,6 +6,7 @@ import json
 import os
 
 # TODO: Use graphql fragments(?) to make reserve queries dynamic (can look up multiple pools in one call)
+# TODO: return block number in addition to timestamp when getting reserves
 
 
 class UniswapV2:
@@ -13,45 +14,82 @@ class UniswapV2:
     endpoint = os.getenv('WEB3_PROVIDER_GRAPHQL')
 
     def get_reserves(self, pool_addr):
-        rawReserves = self.get_raw_reserves(pool_addr)
-        parsedReserves = self.parse_reserves(rawReserves)
+
+        def get_raw_reserves():
+            # Returns pool reserves as of latest block
+            query = """
+            query getReserves($addr: Address!) {
+              block {
+                number
+                pool: account(address: $addr) {
+                  # getReserves() storage slot
+                  reserve: storage(slot: "0x0000000000000000000000000000000000000000000000000000000000000008")
+                }
+              }
+            }
+            """
+            variables = {'addr': pool_addr}
+            resp = requests.post(self.endpoint, json={'query': query, 'variables': variables})
+            json_data = json.loads(resp.text)
+            reserves = json_data['data']['block']['pool']['reserve']
+            return reserves
+
+        def parse_reserves(reserves: str):
+            if reserves is None:
+                return None
+
+            reserves = reserves.replace('0x', '')
+
+            timestampRaw = reserves[0:8]
+            reserve0Raw = reserves[8:36]
+            reserve1Raw = reserves[36:64]
+
+            timestamp = Web3.toInt(hexstr=timestampRaw)
+            reserve0 = Web3.toInt(hexstr=reserve0Raw)
+            reserve1 = Web3.toInt(hexstr=reserve1Raw)
+
+            return {
+                'timestamp': timestamp,
+                'reserve0': reserve0,
+                'reserve1': reserve1
+            }
+
+        rawReserves = get_raw_reserves()
+        parsedReserves = parse_reserves(rawReserves)
         return parsedReserves
 
-    @timer
-    def get_raw_reserves(self, pool_addr):
-        # Returns pool reserves as of latest block
+    def get_pool_tokens(self, pool_addr):
+        # Returns tokens of uni pool
         query = """
         query getReserves($addr: Address!) {
           block {
             number
             pool: account(address: $addr) {
-              reserve: storage(slot: "0x0000000000000000000000000000000000000000000000000000000000000008")
+              token0: storage(slot: "0x0000000000000000000000000000000000000000000000000000000000000006")
+              token1: storage(slot: "0x0000000000000000000000000000000000000000000000000000000000000007")
             }
           }
         }
         """
+
+        # Creates and send GraphQL query
         variables = {'addr': pool_addr}
         resp = requests.post(self.endpoint, json={'query': query, 'variables': variables})
         json_data = json.loads(resp.text)
-        reserves = json_data['data']['block']['pool']['reserve']
-        return reserves
+        tokens = json_data['data']['block']['pool']
 
-    def parse_reserves(self, reserves: str):
-        if reserves is None:
-            return None
+        # Remove zero padding
+        tokens['token0'] = '0x' + tokens['token0'][-40:]
+        tokens['token1'] = '0x' + tokens['token1'][-40:]
+        return tokens
 
-        reserves = reserves.replace('0x', '')
+    def get_factory_pair(self, token0, token1):
+        # given two token addresses, return the address of the pool (using uniswap factory address)
+        # option to only give one address, which will return all pools containing that token addr (there is a seperate contract function that returns this)
+        pass
 
-        timestampRaw = reserves[0:8]
-        reserve0Raw = reserves[8:36]
-        reserve1Raw = reserves[36:64]
-
-        timestamp = Web3.toInt(hexstr=timestampRaw)
-        reserve0 = Web3.toInt(hexstr=reserve0Raw)
-        reserve1 = Web3.toInt(hexstr=reserve1Raw)
-
-        return {
-            'timestamp': timestamp,
-            'reserve0': reserve0,
-            'reserve1': reserve1
-        }
+    def get_pool_volume(self, block_range):
+        # need to look into how to get this...
+        # given a number of blocks, return volume of uni pool from latest block minus block_range to latest block
+        # maybe need to look at transfer events per block; maybe need to standardize in one currency (so volume is comparable across diff pools)
+        pass
